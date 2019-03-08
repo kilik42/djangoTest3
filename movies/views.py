1294bd6dacc18e8fdffb9c94701f3098a471024c
marvin evins <maevins@cps.edu>
2019-03-08 05:12:53 -0600

    db abstraction with django

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 # Create your views here.
 
 def index(request):
-    return HttpResponse("hello world")
+    movies = Movie.objects.all()
+    #Movie.objects.filter(release_year=1984)
+    output = ','.join([m.title for m in movies])
+    return HttpResponse(output)

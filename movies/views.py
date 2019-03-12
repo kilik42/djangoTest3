0907610825d08f9fbc505b09f807f1d67e885473
marvin evins <maevins@cps.edu>
2019-03-12 17:20:40 -0500

    raising exceptions

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Movie
+
 # Create your views here.
 
 def index(request):
@@ -10,4 +11,6 @@ def index(request):
     return render(request, 'movies/index.html', {'movies': movies})
 
 def detail(request, movie_id):
-    return HttpResponse(movie_id)
+
+        movie = get_object_or_404(Movie, pk=movie_id)
+        return render(request, 'movies/detail.html', {'movie': movie})
